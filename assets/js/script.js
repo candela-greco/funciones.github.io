5c9a917b882dfc2d93b100a585a6f71c2a265c5c
candela-greco <candelagreco15@gmail.com>
2024-06-24 23:19:03 -0300

    Subo la primer parte del 4 punto del desafio

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,16 @@ function pintar(color = "green"){
     elemento.style.backgroundColor = color;
 }
 
+
+/*--------- SCRIPT COLORES.HTML ---------*/
+
+function cambiarColor(cuadrado) {
+    cuadrado.target.style.backgroundColor = "black";
+}
+
+document.getElementById("colorAzul").addEventListener("click", cambiarColor);
+document.getElementById("colorRojo").addEventListener("click", cambiarColor);
+document.getElementById("colorVerde").addEventListener("click", cambiarColor);
+document.getElementById("colorAmarillo").addEventListener("click", cambiarColor);
+
+
